24a19246c9852090a4d97c1fdf8200974a2d1b13
tannazma <97254921+tannazma@users.noreply.github.com>
2023-09-17 17:18:32 +0200

    refactor

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ let areaPerSolarPanelInM2
 function calculateEnergy(e) {
     console.log(document.querySelector("#ru").value)
 }
-document.querySelector(".calculate").addEventListener("click", function (e) {
+const form = document.querySelector(".calculate")
+form.addEventListener("click", function (e) {
     e.preventDefault()
     const RU = document.querySelector("#ru")
     const PE = document.querySelector("#pe")
@@ -28,7 +29,7 @@ function validateNumber() {
     }
 
 }
-document.querySelector(".calculate").addEventListener("click", function () {
+form.addEventListener("click", function () {
     const inputFields = document.querySelectorAll("input");
     for (const inputField of inputFields) {
         if (inputField.value === "") {
